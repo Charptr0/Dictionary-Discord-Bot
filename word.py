4f81af5ba98013905ada2adcd5ff880e725b83c2
ptrChar <CharlesOfficial1@hotmail.com>
2021-03-19 15:40:33 -0400

    added word class

---
+++ word.py
@@ -0,0 +1,8 @@
+class Word:
+    def __init__(self, name, partOfSpeech, definition : list, synonyms : list, antonyms : list):
+        self.name = name
+        self.partOfSpeech = partOfSpeech
+        self.defintion = definition
+        self.synonyms = synonyms
+        self.antonyms = antonyms
+
