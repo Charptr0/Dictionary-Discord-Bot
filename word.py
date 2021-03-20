f51c0dd3bf5424b4e066cb37c14b6fc48a7f576e
ptrChar <CharlesOfficial1@hotmail.com>
2021-03-20 01:23:34 -0400

    removed

--- word.py
+++
@@ -1,8 +0,0 @@
-class Word:
-    def __init__(self, name, partOfSpeech, definition : list, synonyms : list, antonyms : list):
-        self.name = name
-        self.partOfSpeech = partOfSpeech
-        self.defintion = definition
-        self.synonyms = synonyms
-        self.antonyms = antonyms
-
