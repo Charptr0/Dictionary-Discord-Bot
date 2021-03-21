682011efc69fe00a9c8b03cf82a46c7b48b7ed49
ptrChar <CharlesOfficial1@hotmail.com>
2021-03-21 14:08:27 -0400

    +embed message for error messages +embed messages for definitions+pos

--- bot_main.py
+++ bot_main.py
@@ -8,6 +8,28 @@ token = ""
 bot = commands.Bot(command_prefix= "db!")
 bot.remove_command("help")
 
+
+def createWordEmbed(word): #If the word exists, create an embed message that contains the name, pos, definition, synonyms and antonyms of the word
+    embed_message = discord.Embed(title="Dictionary Bot", description="Here is what I found:", colour=discord.Colour.blue())
+    embed_message.add_field(name="Word", value=word.name, inline=False)
+    embed_message.add_field(name="Part of Speech", value=word.partOfSpeech(), inline=False)
+    embed_message.add_field(name="Definitions", value=word.definitions(), inline=False)
+    return embed_message
+
+def createWordErrorEmbed(error_message): #If an error occurs, create an embed message to inform the user
+    embed_message = discord.Embed(title="Dictionary Bot", description="Something went wrong and I cannot find what you are looking for", colour=discord.Colour.orange())
+    embed_message.add_field(name="Error Message:", value=error_message, inline=False)
+
+    if "404" in error_message:
+        embed_message.add_field(name="Possible Soultion:", value='''Please check your spelling of the word and try again.\nTry using "db!udefine" instead, the word you are looking for does not exist on dictionary.com''', inline=False)
+
+    elif "No definitions found" in error_message:
+        embed_message.add_field(name="Possible Soultion:", value='''No definitions exist for this word according to Dictionary.com''', inline=False)
+
+    embed_message.set_footer(text="Please contact the server mods if you are having trouble")
+
+    return embed_message
+
 @bot.event
 async def on_ready():
     print("The bot is ready")
@@ -15,10 +37,9 @@ async def on_ready():
 @bot.command("define")
 async def define(ctx, unknownWord):
     word = dictionary.getHTML(unknownWord)
-    if word == "404" or word.definitions() == "No definitions found":
-        await ctx.send("error")
+    if word == "404 Webpage cannot be found" or word.definitions() == "No definitions found":
+        await ctx.send(embed=createWordErrorEmbed(word))
 
-    else: await ctx.send(str(word.definitions()))
-
+    else: await ctx.send(embed=createWordEmbed(word))
 
 bot.run(token)
