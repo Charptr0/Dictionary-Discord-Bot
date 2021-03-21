b46699598481c47f0d0e4accf70c9307ced84ddf
ptrChar <CharlesOfficial1@hotmail.com>
2021-03-21 01:08:30 -0400

    define command added, only gives definitions.

--- bot_main.py
+++ bot_main.py
@@ -1,18 +1,24 @@
 import discord
+from discord import embeds
 from discord.ext import commands
 import dictionary
 
 token = ""
 
-bot = commands.Bot(command_prefix= "!db")
+bot = commands.Bot(command_prefix= "db!")
 bot.remove_command("help")
-
 
 @bot.event
 async def on_ready():
     print("The bot is ready")
 
 @bot.command("define")
-async def define(ctx):
-    pass
+async def define(ctx, unknownWord):
+    word = dictionary.getHTML(unknownWord)
+    if word == "404" or word.definitions() == "No definitions found":
+        await ctx.send("error")
 
+    else: await ctx.send(str(word.definitions()))
+
+
+bot.run(token)
